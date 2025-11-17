21aca96116ddfb9dd844ecdf84d18f4785834281
albertdevi <albertdemures@gmail.com>
2025-11-17 22:32:31 +0100

    add song and bugfixing

---
+++ staff/albert-demures/playground/js/abstraction.js
@@ -0,0 +1,10 @@
+var DiosOlimpo = function(nombre, dominio ){
+this.nombre = nombre
+this.dominio = dominio
+
+}
+
+var zeus = new DiosOlimpo ('Zeus', 'Rayo')
+var poseidon = new DiosOlimpo ('Poseidon', 'mar')
+
+var diosesOlimpo = new Array (zeus, poseidon)
