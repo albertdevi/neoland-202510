8842b66d84fa2825f722586890760bd97a558ae9
albertdevi <albertdemures@gmail.com>
2026-01-12 22:03:10 +0100

    add links, form valifation function, show and hide password function

--- staff/albert-demures/product/app.1/main.jsx
+++ staff/albert-demures/product/app.1/main.jsx
@@ -5,20 +5,30 @@ root.render(<App />)
 const useState = React.useState
 
 function App() {
+    console.log('App -> call')
+
     const [view, setView] = useState('landing')
+    const [message, setMessage] = useState('')
+    const [passwordType, setPasswordType] = useState('password')
+    const [passwordRepeatType, setPasswordRepeatType] = useState('password')
 
     const handleLoginClick = event => {
         event.preventDefault()
+        setMessage('')
 
+        setPasswordType('password')
+        setPasswordRepeatType('password')
         setView('login')
     }
 
     const handleRegisterClick = event => {
         event.preventDefault()
-
+        setMessage('')
+        
+        setPasswordType('password')
+        setPasswordRepeatType('password')
         setView('register')
     }
-
 
     const handleHomeClick = event => {
         event.preventDefault()
@@ -31,6 +41,65 @@ function App() {
 
         setView('add-pet')
     }
+
+    const handleLoginSubmit = event => {
+        event.preventDefault()
+
+        const form = event.target
+
+        const username = form.username.value
+        const password = form.password.value
+
+        try {
+            logic.loginUser(username, password)
+
+            form.reset()
+
+            setView('home')
+
+            setMessage('')
+        } catch (error) {
+            setMessage(error.message)
+        }
+    }
+
+    const handleRegisterSubmit = event => {
+
+        event.preventDefault()
+
+        const form = event.target
+
+        const name = form.name.value
+        const email = form.email.value
+        const username = form.username.value
+        const password = form.password.value
+        const passwordRepeat = form.passwordRepeat.value
+
+        try {
+            logic.registerUser(name, email, username, password, passwordRepeat)
+
+            form.reset()
+
+            setView('login')
+            setMessage('')
+        } catch (error) {
+            setMessage(error.message)
+        }
+    }
+
+    const handleTogglePasswordClick = event => {
+        event.preventDefault()
+
+        setPasswordType(passwordType === 'password'? 'text' : 'password')
+    }
+
+    const handleTogglePasswordRepeatClick = event => {
+        event.preventDefault()
+
+        setPasswordRepeatType(passwordRepeatType === 'password'? 'text' : 'password')
+    }
+
+    console.log('App -> render')
 
     // landing
     if (view === 'landing')
@@ -46,7 +115,6 @@ function App() {
             </nav>
         </div>
 
-
     //login
     if (view === 'login')
         return <div className="flex flex-col items-center justify-center min-h-screen">
@@ -54,24 +122,22 @@ function App() {
 
             <h2 className="text-3xl font-semibold text-blue-500 text-center mt-4 tracking-tight">Login</h2>
 
-            <form className="flex flex-col gap-4 w-full max-w-sm mx-auto mt-6">
+            <form className="flex flex-col gap-4 w-full max-w-sm mx-auto mt-6" onSubmit={handleLoginSubmit}>
                 <label className="text-m text-gray-600 mt-2" htmlFor="username">Username</label>
                 <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" id="username" type="text" />
 
                 <label className="text-m text-gray-600 mt-2" htmlFor="password">Password</label>
-                <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" id="password" type="password" />
+                <input className= {'border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition' + (passwordType === 'password'? ' bg-white-100' : ' bg-yellow-100')} id="password" type= {passwordType} />
 
-                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button">Show</button>
+                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button" onClick={handleTogglePasswordClick} > {passwordType === 'password'? 'Show' : 'Hide'}</button>
 
-                <button className="bg-blue-600 text-white font-semibold py-2 px-16 rounded-lg shadow hover:bg-blue-700 transition-colors duration-200 self-center" onClick={handleHomeClick}>Login</button>
+                <button className="bg-blue-600 text-white font-semibold py-2 px-16 rounded-lg shadow hover:bg-blue-700 transition-colors duration-200 self-center" >Login</button>
             </form>
 
             <a className="text-gray-700 text-lg leading-loose max-w-md mx-auto mt-4 text-center cursor-pointer underline font-bold" onClick={handleRegisterClick}>Register</a>
 
-            <p className="text-red-600 text-sm mt-2 font-medium"></p>
+            <p className="text-red-600 text-sm mt-2 font-medium"> {message} </p>
         </div>
-
-
 
     // Register
     if (view === 'register')
@@ -81,7 +147,7 @@ function App() {
 
             <h2 className="text-3xl font-semibold text-blue-500 text-center mt-4 tracking-tight">Register</h2>
 
-            <form className="flex flex-col gap-4 w-full max-w-sm mx-auto mt-6">
+            <form className="flex flex-col gap-4 w-full max-w-sm mx-auto mt-6" onSubmit={handleRegisterSubmit}>
                 <label className="text-m text-gray-600 mt-2" htmlFor="name">Name</label>
                 <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" id="name" type="text" />
 
@@ -91,22 +157,23 @@ function App() {
                 <label className="text-m text-gray-600 mt-2">Username</label>
                 <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" id="username" type="text" />
 
-                <label className="text-m text-gray-600 mt-2">Password</label>
+                <label className="text-m text-gray-600 mt-2" htmlFor='password'>Password</label>
 
-                <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" type="password" />
+                <input className= {'border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition' + (passwordType === 'password'? ' bg-white-100' : ' bg-yellow-100')}  id="password" type={passwordType} />
 
-                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button">Show</button>
+                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button" onClick={handleTogglePasswordClick}>{passwordType === 'password'? 'show' : 'Hide'}</button>
 
-                <label className="text-m text-gray-600 mt-2" htmlFor="repeatPassword">Repeat Password</label>
-                <input className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" id="repeatPassword" type="password" />
-                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button">Show</button>
+                <label className="text-m text-gray-600 mt-2" htmlFor="passwordRepeat">Repeat Password</label>
+                <input className= {'border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition' + (passwordRepeatType === 'password'? ' bg-white-100' : ' bg-yellow-100')}  id="passwordRepeat" type={passwordRepeatType} />
+
+                <button className="w-24 bg-gray-600 text-white font-semibold py-1 px-0 rounded-lg shadow hover:bg-gray-700 transition-colors duration-200 self-end" type="button" onClick={handleTogglePasswordRepeatClick}>{passwordRepeatType === 'password'? 'show' : 'Hide'}</button>
 
                 <button className="bg-blue-600 text-white font-semibold py-2 px-16 rounded-lg shadow hover:bg-blue-700 transition-colors duration-200 self-center" type="submit">Register</button>
             </form>
 
             <a href="" className="text-gray-700 text-lg leading-loose max-w-md mx-auto mt-4 text-center cursor-pointer underline font-bold" onClick={handleLoginClick}>Login</a>
 
-            <p className="text-red-600 text-sm mt-2 font-medium"></p>
+            <p className="text-red-600 text-sm mt-2 font-medium">{message}</p>
 
         </div>
 
@@ -189,7 +256,6 @@ function App() {
 
         </div>
 
-
     //pet
     if (view === 'pet')
         return <div className="flex flex-col gap-5 items-center justify-center min-h-screen" >
@@ -198,5 +264,4 @@ function App() {
             <h2 className="text-3xl font-semibold text-blue-500 text-center mt-4 tracking-tight">Pet details</h2>
 
         </div>
-
 }
