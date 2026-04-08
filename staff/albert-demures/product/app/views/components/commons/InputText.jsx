f8ef5b7fbe0735de335bfc2daf4cc80a656fdc0c
albertdevi <albertdemures@gmail.com>
2026-04-08 19:39:12 +0200

    update input text

--- staff/albert-demures/product/app/views/components/commons/InputText.jsx
+++ staff/albert-demures/product/app/views/components/commons/InputText.jsx
@@ -1,7 +1,7 @@
 export function InputText({ alias, autoComplete, type, className, defaultValue, step}) {
     return <input id={alias} name={alias} autoComplete={autoComplete || alias} type={type} className={`
-                border border-gray-300 rounded-lg px-3 py-2 shadow-sm
+                  border border-gray-300 rounded-lg px-3 py-2 shadow-sm
                 focus:outline-none focus:ring-2 focus:ring-blue-500
-                focus:border-blue-500 transition}
+                focus:border-blue-500 transition
                 ${className}`} defaultValue= {defaultValue} step={step} />
 }
